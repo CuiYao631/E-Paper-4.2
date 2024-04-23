6f5f177c2d6bc2c761e5e1ef967a634f15cb7283
CuiYao <631836754@qq.com>
2024-04-23 16:21:33 +0800

    udpate

--- E-Paper-4.2.ino
+++ E-Paper-4.2.ino
@@ -4,14 +4,25 @@
  * @date: 2021-08-06
  * @version: 0.0.1
  */
-
+ 
+#include <Adafruit_GFX.h>
 #include <GxEPD2_2IC_BW.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
 #include "WiFi.h"
 #include "btn.h"
+#include <U8g2_for_Adafruit_GFX.h>//中文库
 
 // 初始化墨水屏
-GxEPD2_2IC_BW<GxEPD2_2IC_420_A03, GxEPD2_2IC_420_A03::HEIGHT> display(GxEPD2_2IC_420_A03(/*CS=5*/ 33,/*CS1=*/32,/*DC=*/ 26, /*RST=*/ 25, /*BUSY=*/ 14)); // GDEH042A03-A1
+GxEPD2_2IC_BW<GxEPD2_2IC_420_A03, GxEPD2_2IC_420_A03::HEIGHT> display(GxEPD2_2IC_420_A03(/*CS=*/ 27,/*CS1=*/26,/*DC=*/ 5, /*RST=*/ 17, /*BUSY=*/ 16)); // GDEH042A03-A1
+U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;
+
+// 定义进度条参数
+int16_t progressBarX = 20;
+int16_t progressBarY = 40;
+int16_t progressBarWidth = 200;
+int16_t progressBarHeight = 20;
+int16_t progress = 0;
+
 
 void setup()
 {
@@ -19,23 +30,39 @@ void setup()
   Serial.begin(115200);
   // 初始化墨水屏
   display.init(115200);
+  u8g2Fonts.begin(display);
+  u8g2Fonts.setFontDirection(0);      
+  u8g2Fonts.setForegroundColor(GxEPD_BLACK);             // 设置前景色
+  u8g2Fonts.setBackgroundColor(GxEPD_WHITE);             // 设置背景色
+  u8g2Fonts.setFont(u8g2_font_wqy16_t_gb2312a);
+  //开机加载
+
   //初始化WIFI
-  wifi_connect();
+  //wifi_connect();
   //初始化按键
-  BtnInit();
+  //BtnI nit();
   //开机画面
   
   //显示Hello World
-  helloWorld();
+  // helloWorld();
+  // delay(1000);
+  // helloWorld();
+  // delay(1000);
+  // helloWorld();
+  // delay(1000);
+  // 清空屏幕
+  display.fillScreen(GxEPD_WHITE);
+  DrawRectangle();
+  DrawCircle();
   //显示屏休眠
-  //display.hibernate();
+  display.hibernate();
 }
 
 const char HelloWorld[] = "Hello World!";
 
 void helloWorld()
 {
-  display.setRotation(1);
+  display.setRotation(0);
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(GxEPD_BLACK);
   int16_t tbx, tby; uint16_t tbw, tbh;
@@ -54,4 +81,75 @@ void helloWorld()
   while (display.nextPage());
 }
 
-void loop() {};
+//绘制矩形
+void DrawRectangle(){
+  // 设置矩形的坐标和大小
+  int16_t x = 10;
+  int16_t y = 10;
+  int16_t width = 50;
+  int16_t height = 50;
+
+  // 设置矩形的颜色
+  uint16_t color = GxEPD_BLACK;
+
+  // 绘制矩形
+  display.drawRect(x, y, width, height, color);
+  u8g2Fonts.setCursor(5, 25);
+  u8g2Fonts.println("不要温顺的走进那个良夜，");
+  u8g2Fonts.println("激情不能被消沉的暮色淹没，");
+  u8g2Fonts.println("咆哮吧，咆哮，痛斥那光的退缩。");
+  u8g2Fonts.println("智者在临终的时候，对黑暗妥协，");
+  u8g2Fonts.println("是因为它们的语言已黯然失色，");//");
+  u8g2Fonts.println("它们不想被夜色迷惑，");
+  u8g2Fonts.println("咆哮吧，咆哮，痛斥那光的退缩。");
+
+  // 更新显示
+  display.display();
+}
+
+//绘制圆形
+void DrawCircle(){
+  int16_t x = display.width() / 2; // 屏幕中心点的 x 坐标
+  int16_t y = display.height() / 2; // 屏幕中心点的 y 坐标
+  int16_t radius = 30; // 圆形的半径
+
+  // 设置圆形的颜色
+  uint16_t color = GxEPD_BLACK;
+
+  // 绘制圆形
+  display.drawCircle(x, y, radius, color);
+
+  // 更新显示
+  display.display(true);
+   // 等待一段时间
+  delay(5000);
+}
+
+
+void loop() {
+ // 设置进度条背景颜色
+  uint16_t backgroundColor = GxEPD_WHITE;
+  // 绘制进度条背景
+  display.drawRect(progressBarX, progressBarY, progressBarWidth, progressBarHeight, GxEPD_BLACK);
+
+  // 设置进度条填充颜色
+  uint16_t progressColor = GxEPD_BLACK;
+  // 计算填充矩形的宽度
+  int16_t progressWidth = map(progress, 0, 100, 0, progressBarWidth);
+  // 绘制进度条填充矩形
+  display.fillRect(progressBarX, progressBarY, progressWidth, progressBarHeight, progressColor);
+
+  // 更新显示
+  display.display(true);
+
+  // 增加进度
+  progress += 5;
+  if (progress > 100) {
+
+    progress = 0;
+  }
+
+  // 等待一段时间
+  delay(200);
+
+};
